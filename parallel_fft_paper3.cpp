03239536b61f6c1dc0d56115c52fa5009803c42a
artfai <134277184+artfai@users.noreply.github.com>
2023-06-08 10:58:50 +0200

    Update parallel_fft_paper3.cpp

--- parallel_fft_paper3.cpp
+++ parallel_fft_paper3.cpp
@@ -1,2 +1,9 @@
 // Implementation of a parallel radix-2 algorithm folloing Paper (3) : "A parallel FFT on an MIMD machine" by Amir AVERBUCH
 
+Complex bit_reversal(double x, int n) {
+    Complex res[n]; 
+    for (int i=0; i<n/2; i++) {
+            res[i] = x[n-i-1]; 
+    } 
+    return res; 
+}

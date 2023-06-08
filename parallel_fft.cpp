682db3f8aed2047869ef7c41b772985cae014c56
alexchristlieb <alexchristlieb@gmail.com>
2023-06-08 10:16:20 +0200

    Debugging, but without success yet

--- parallel_fft.cpp
+++ parallel_fft.cpp
@@ -8,7 +8,7 @@
 
 typedef std::complex<double> Complex;
 
-std::vector<Complex> fft_parallel(const std::vector<Complex>& x) {
+std::vector<Complex> fft_forward(const std::vector<Complex>& x) {
     int N = x.size();
 
     // Trivial size-1 DFT base case
@@ -27,49 +27,35 @@ std::vector<Complex> fft_parallel(const std::vector<Complex>& x) {
 
     // Recursion
     std::vector<Complex> transformed(N);
-    std::vector<Complex> evenTransformed = fft_parallel(even);
-    std::vector<Complex> oddTransformed = fft_parallel(odd);
+    std::vector<Complex> evenTransformed = fft_forward(even);
+    std::vector<Complex> oddTransformed = fft_forward(odd);
 
     // Combine the even and odd arrays
     for (int k = 0; k < N / 2; k++) {
-        Complex factor = std::polar(1.0, -2.0 * M_PI * k / N) * oddTransformed[k];
-        transformed[k] = evenTransformed[k] + factor;
-        transformed[k + N / 2] = evenTransformed[k] - factor;
+        Complex coeff (cos(2*M_PI*k/N), sin(2*M_PI*k/N));
+        Complex q = coeff * oddTransformed[k];
+        transformed[k] = evenTransformed[k] + q;
+        transformed[k + (N/2)] = evenTransformed[k] - q;
     }
 
     return transformed;
 }
 
-std::vector<Complex> inverse_fft_parallel(const std::vector<Complex>& x) {
-    int N = x.size();
+std::vector<Complex> inverse_fft(const std::vector<Complex>& x) {
+    std::vector<Complex> conj_x(x.size());
 
-    // Trivial size-1 DFT base case
-    if (N <= 1) {
-        return x;
+    for (int i = 0; i < x.size(); i++) {
+        conj_x[i] = std::conj(x[i]);
     }
 
-    // Divide x into even and odd indices
-    std::vector<Complex> even;
-    std::vector<Complex> odd;
+    std::vector<Complex> transformed = fft_forward(conj_x);
 
-    for (size_t i = 0; i < N; i += 2) {
-        even.push_back(x[i]);
-        odd.push_back(x[i + 1]);
+    int N = transformed.size();
+    for (int i = 0; i < transformed.size(); i++) {
+        transformed[i] = std::conj(transformed[i])/(Complex)N;
     }
 
-    // Recursion
-    std::vector<Complex> transformed(N);
-    std::vector<Complex> evenTransformed = inverse_fft_parallel(even);
-    std::vector<Complex> oddTransformed = inverse_fft_parallel(odd);
-
-    // Combine the even and odd arrays
-    for (int k = 0; k < N / 2; k++) {
-        Complex factor = std::polar(1.0, 2.0 * M_PI * k / N) * oddTransformed[k];
-        transformed[k] = evenTransformed[k] + factor;
-        transformed[k + N / 2] = evenTransformed[k] - factor;
-    }
-
-    return transformed;
+    return transformed;   
 }
 
 void parallel_multiply(const std::vector<Complex>& a, const std::vector<Complex>& b, std::vector<Complex>& result, int start, int end) {
@@ -78,77 +64,71 @@ void parallel_multiply(const std::vector<Complex>& a, const std::vector<Complex>
     }
 }
 
-std::vector<double> parallel_fft_multiply(const std::vector<double>& res, const std::vector<double>& coefficients) {
+std::vector<Complex> parallel_fft_multiply(std::vector<Complex>& x, const std::vector<Complex>& coefficients) {
     // Final function: perform the forward FFT computation on the vector of input values and the coefficients, multiply  
     // the transformed arrays in parallel, and apply the inverse FFT to obtain the final result.
 
-    int N = res.size();
+    int N = x.size();
 
-    std::vector<Complex> transformed_res(N);
-    std::vector<Complex> transformed_coefficients(N);
+    std::vector<Complex> res(N);
+    // std::vector<Complex> transformed_coefficients(N);
     std::vector<Complex> multiplied_transform(N);
     std::vector<Complex> multiplied_res(N);
 
-    // Convert input signals to complex numbers
-    for (int i = 0; i < N; i++) {
-        transformed_res[i] = Complex(res[i], 0.0);
-        transformed_coefficients[i] = Complex(coefficients[i], 0.0);
-    }
-
     // Perform FFT on both signal and coefficients
-    transformed_res = fft_parallel(transformed_res);
-    transformed_coefficients = fft_parallel(transformed_coefficients);
+    res = fft_forward(x);
 
-    int num_threads = std::thread::hardware_concurrency();
-    std::vector<std::thread> workers(num_threads);
-    int block_size = N / num_threads;
+    // PARALLEL VERSION
+    // int num_threads = 4;
+    // std::vector<std::thread> workers(num_threads);
+    // int block_size = N / num_threads;
 
-    // Multiply transformed arrays in parallel
-    for (int t = 0; t < num_threads; t++) {
-        int start = t * block_size;
-        int end;
-        if (t == num_threads - 1) {
-            end = N;
-        } else {
-            end = start + block_size;
-        }
-        workers[t] = std::thread(&parallel_multiply, std::ref(transformed_res), std::ref(transformed_coefficients), std::ref(multiplied_transform), start, end);
+    // NOTE: even without parallelization it does not work
+    for (int i = 0; i < res.size(); i++) {
+        multiplied_transform[i] = res[i] * coefficients[i];
     }
 
-    // Wait for all threads to finish
-    for (int t = 0; t < num_threads; t++) {
-        workers[t].join();
-    }
+    // PARALLEL VERSION 
+    // for (int t = 0; t < num_threads; t++) {
+    //     int start = t * block_size;
+    //     int end;
+    //     if (t == num_threads - 1) {
+    //         end = N;
+    //     } else {
+    //         end = start + block_size;
+    //     }
+    //     std::cout << "Thread " << t <<  "start: " <<  start << "end: " << end << std::endl;
+    //     workers[t] = std::thread(&parallel_multiply, std::cref(res), std::cref(coefficients), std::ref(multiplied_transform), start, end);
+    // }
+
+    // // Wait for all threads to finish
+    // for (int t = 0; t < num_threads; t++) {
+    //     workers[t].join();
+    // }
 
     // Perform inverse FFT on the multiplied transform
-    multiplied_res = inverse_fft_parallel(multiplied_transform);
+    multiplied_res = inverse_fft(multiplied_transform);
 
-    // Extract the real part of the result
-    std::vector<double> result(N);
-    for (int i = 0; i < N; i++) {
-        //result[i] = multiplied_res[i].real();
-        result[i] = std::real(multiplied_res[i]);
-    }
-
-    return result;
+    return multiplied_res;
 }
 
 int main() {
     // Test with values 1,2,3,4
-    std::vector<double> signal; 
-    const double temp1[] = {1.0,2.0,3.0,4.0};
+    std::vector<Complex> x; 
+    const double temp1[] = {1.0, 2.0, 3.0, 4.0};
     for (size_t i = 0; i < 4; i += 1) {
-            signal.push_back(temp1[i]);
+            x.push_back(temp1[i]);
     } 
-    std::vector<double> coefficients; // Coefficient array
-    const double temp2[] = {1, 2, 1, 0};
+    std::vector<Complex> coefficients; // Coefficient array
+    
     for (size_t i = 0; i < 4; i += 1) {
-            coefficients.push_back(temp2[i]);
+        double theta = -2.0 * M_PI * i / 4;
+        coefficients.push_back(Complex(std::cos(theta), std::sin(theta)));
     }
 
-    std::vector<double> result = parallel_fft_multiply(signal, coefficients);
+    std::vector<Complex> result = parallel_fft_multiply(x, coefficients);
 
-    for (double value : result) {
+    for (Complex value : result) {
         std::cout << value << " ";
     }
     std::cout << std::endl;

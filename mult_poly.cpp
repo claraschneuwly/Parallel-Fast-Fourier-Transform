784616362357cfcbd511bb2a5e3e51905f58bfaa
artfai <134277184+artfai@users.noreply.github.com>
2023-06-09 22:42:42 +0200

    Update mult_poly.cpp

--- mult_poly.cpp
+++ mult_poly.cpp
@@ -8,19 +8,20 @@
 #include <fstream>
 
 
-void mult_poly_bis(){
-}; 
-
-
 void multi_poly(complex<double>* res, complex<double>* p1, complex<double>* p2, int n){
     complex<double> poly1[2 * n]
     complex<double> poly2[2 * n]
     complex<double> result_poly[2 * n];
-    parallel_fft(poly1, p1, 2 * n);
-    parallel_fft(poly2, p2, 2 * n);
-    mult_poly_bis(result, poly1, poly2, 2 * n);
-    inverse_fft(res, result_poly, 2 * n);
-};
+    #Apply fft to both polynomials 
+    parallel_fft(poly1, p1, 2 * n);                    
+    parallel_fft(poly2, p2, 2 * n); 
+    #multiply the coefficients of the transformed polynomials
+    for (i = 0; i<n; i++){
+        result_poly[i] = poly1[i]*poly2[i]; 
+    }
+    #Apply the inverse fft to the multiplied polynomial to obtain the final polynomial 
+    inverse_fft(res, result_poly, 2 * n); 
+}; 
 
 int main(){
     int n = 4;

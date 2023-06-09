7fc9e2ce279454009a13c1854c5823b096971689
artfai <134277184+artfai@users.noreply.github.com>
2023-06-09 22:34:37 +0200

    Update mult_poly.cpp

--- mult_poly.cpp
+++ mult_poly.cpp
@@ -8,13 +8,15 @@
 #include <fstream>
 
 
+void mult_poly_bis()
+
 void multi_poly(complex<double>* res, complex<double>* p1, complex<double>* p2, int n){
     complex<double> poly1[2 * n]
     complex<double> poly2[2 * n]
     complex<double> result_poly[2 * n];
     parallel_fft(poly1, p1, 2 * n);
     parallel_fft(poly2, p2, 2 * n);
-    mult_poly(result, poly1, poly2, 2 * n);
+    mult_poly_bis(result, poly1, poly2, 2 * n);
     inverse_fft(res, result_poly, 2 * n);
 };
 
